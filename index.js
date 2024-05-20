97512a57e7694ac37d0ccfd4a80e2501d953ea01
Md Antor <69227191+Antor1155@users.noreply.github.com>
2024-05-20 15:40:39 +0600

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   console.log("request got");
   const secret = process.env.SECRET;
-  res.send("this is the changed code : " + secret);
+  res.send("this is the changed code : " + secret  + " RUNNING ON PORT : " + port);
 });
 
 app.listen(port, () => {
